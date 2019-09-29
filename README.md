d3151b138326548aa8aca87346d91f949482f5f1
Erika Ogino <erika.ogino.20@dartmouth.edu>
2019-09-29 19:04:53 -0400

    readme and scraping

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hack a thing 2 -- Erika and Kristie
+## Description 
+
+
+## Who did what 
+
+### Erika 
+Followed webscraping tutorial 
+https://www.youtube.com/watch?v=XQgXKtPSzUI
+
+
+### Kristie 
+
+
+## What we learned 
+
+
+## How does this hack inspire you or relate to your possible project ideas?
+
+
+## What didn't work 
---
+++ webscraping_amazon.py
@@ -0,0 +1,27 @@
+# Webscraping for amazon URL 
+
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+
+my_url = 'https://www.amazon.com/s?k=alexa&ref=nb_sb_noss_2'
+
+# opening up connection and grabbing page 
+uClient = uReq(my_url)
+page_html= uClient.read()
+uClient.close()
+
+# parsing through the page 
+page_soup= soup(page_html,"html.parser")
+
+
+# gets each product 
+containers= page_soup.findAll("div",{"class":"sg-col-4-of-12 sg-col-8-of-16 sg-col-16-of-24 sg-col-12-of-20 sg-col-24-of-32 sg-col sg-col-28-of-36 sg-col-20-of-28"})
+
+print(len(containers))
+
+
+#remove amazon header
+containers= containers[1:]
+container=containers[1]
+container.a.span.text
